6d2d136af7679e4b2ec8608baa019797a2859e2b
rah1236 <39041279+rah1236@users.noreply.github.com>
2023-03-23 21:26:00 -0700

    added watchdog timers

--- pleaseDontMakeMeBroke.py
+++ pleaseDontMakeMeBroke.py
@@ -8,11 +8,13 @@ auth_client =   coinbasepro.AuthenticatedClient(api_key,api_secret,passphrase)
 
 
 
-orderCompletePeriod = 10 #number of seconds to wait before checking api if order is complete
-loopPeriod = 60         #number of seconds to wait for going thru the whole loop
+orderCompletePeriod = 5 #number of seconds to wait before checking api if order is complete
+loopPeriod = 10         #number of seconds to wait for going thru the whole loop
 
 loopEnable = True        #toggles trading loop
 
+buyOrderTimeout = 60   #number of times to loop through orderCompletePeriod before giving up and canceling order
+buyOrderTimeoutWatchdog = 0
 
 lastBuySize = 0
 lastSellSize = 0
@@ -23,6 +25,8 @@ lastSellPrice = 0
 lastBuyPrice = 0
 buyPrice = 0
 sellPrice = 0
+
+buy_order = {'fill_fees':0}
 
 productID = 'ETH-USD'
 
@@ -35,6 +39,15 @@ def getETHBalance():
 def getUSDBalance():
     return((auth_client.get_account(acct_id_USD)['available']))
 
+def getWatchdog(counter, countTo):
+    counter += 1
+    return (counter == countTo)
+
+def resetWatchdog(counter):
+    counter = 0    
+
+
+
 # list = auth_client.get_accounts()
 
 # for thing in list:
@@ -46,54 +59,52 @@ while loopEnable:
     USDBalance = getUSDBalance()
     ETHBalance = getETHBalance()
 
-    print("Current ETH Balance: {}".format(ETHBalance))
-    print("Current USD Balance: ${}".format(USDBalance))
-
     ticker = public_client.get_product_ticker(productID)
     buyPrice = float(ticker['price'])
     sellPrice = float(ticker['price'])
 
+    buySize  = round(random.uniform(0.003, 0.02),5) #coinbase gets upset if you try to have more accuracy than 1e-8
+    sellSize = round(random.uniform(0.005, float(ETHBalance)), 5)
+    print("-------------------------------------------")
+    print("Current ETH Balance: {}".format(ETHBalance))
+    print("Current USD Balance: ${}".format(USDBalance))
+    print("ETH-USD: {}".format(buyPrice))
 
-    if (random.choice([True,False]) and USDBalance < 125.0 and USDBalance > 10.0): #buying
-        buySize = round(random.uniform(0.003, 0.02),5) #coinbase gets upset if you try to have more accuracy than 1e-8
-        while(buySize*buyPrice > USDBalance):
-            buySize = round(random.uniform(0.003, 0.02),5)
+    if (random.choice([True,False]) and buySize*buyPrice < USDBalance): #buying
         print('Buying {} ETH at ${} per Eth. (${})'.format(buySize, buyPrice, float(buyPrice)*float(buySize)))
         buy_order = auth_client.place_limit_order(product_id=productID, side='buy', price=buyPrice, size=buySize)
-        if (auth_client.get_order((buy_order['id']))['filled_size'] > 0):#auth_client.get_order((buy_order['id']))['status'] == 'done'): #this is fucking ratchet
+        if (auth_client.get_order((buy_order['id']))['filled_size'] > 0): #this is fucking ratchet
              print()
              print('!!BOUGHT {} ETH at ${} per Eth. (${})'.format(buySize, buyPrice, round(float(buyPrice)*float(buySize),2)))
-             lastBuyPrice = buyPrice
+             lastBuyPrice = buyPrice 
              lastBuySize = buySize
         else:
-            while(auth_client.get_order((buy_order['id']))['filled_size'] <= 0):
+            while(getWatchdog(buyOrderTimeoutWatchdog,buyOrderTimeout) or auth_client.get_order((buy_order['id']))['filled_size'] <= 0):
                 print("waiting for buy order to complete")
                 time.sleep(orderCompletePeriod)
+            resetWatchdog(buyOrderTimeoutWatchdog)
             print()
             print('!!Bought {} ETH at ${} per Eth. (${})'.format(buySize, buyPrice, round(float(buyPrice)*float(buySize),2)))
+            lastBuyPrice = buyPrice 
+            lastBuySize = buySize
+   
+    if(sellSize < ETHBalance and sellSize*sellPrice >= lastBuyPrice*lastBuySize+2*float(buy_order['fill_fees'])): #selling
+        print('Selling {} ETH at ${} per Eth. (${})'.format(sellSize, sellPrice, float(sellPrice)*float(sellSize)))
+        sell_order = auth_client.place_limit_order(product_id=productID, side='sell', price=sellPrice, size=sellSize)
+        if (auth_client.get_order((sell_order['id']))['filled_size'] > 0): #this is fucking ratchet
+            print()
+            print('!!SOLD {} ETH at ${} per Eth. (${})'.format(sellSize, sellPrice, round(float(sellPrice)*float(sellSize),2)))
             lastBuyPrice = buyPrice
             lastBuySize = buySize
-    else:
-        print("Balance is too high/low: it's currently sitting at ${}".format(USDBalance))
-    
-    if(random.choice([True,False])): #selling
-        sellSize = round(random.uniform(0.005, 0.02), 5)
-        if (sellSize < getETHBalance()):
-            print('Selling {} ETH at ${} per Eth. (${})'.format(sellSize, sellPrice, float(sellPrice)*float(sellSize)))
-            sell_order = auth_client.place_limit_order(product_id=productID, side='sell', price=sellPrice, size=sellSize)
-            if (auth_client.get_order((sell_order['id']))['filled_size'] > 0): #this is fucking ratchet
-                print()
-                print('!!SOLD {} ETH at ${} per Eth. (${})'.format(sellSize, sellPrice, round(float(sellPrice)*float(sellSize),2)))
-                lastBuyPrice = buyPrice
-                lastBuySize = buySize
-            else:
-                while(auth_client.get_order((sell_order['id']))['filled_size'] <= 0):
-                    print("waiting for sell order to complete")
-                    time.sleep(orderCompletePeriod) #self rate limitting, dont want to sell my whole account
-                print()
-                print('!!SOLD {} ETH at ${} per Eth. (${})'.format(sellSize, sellPrice, round(float(sellPrice)*float(sellSize),2)))
-                lastSellPrice = sellPrice
-                lastBuySize = sellSize
+        else:
+            while(getWatchdog(buyOrderTimeoutWatchdog,buyOrderTimeout) or auth_client.get_order((sell_order['id']))['filled_size'] <= 0):
+                print("waiting for sell order to complete")
+                time.sleep(orderCompletePeriod) #self rate limitting, dont want to sell my whole account
+            resetWatchdog(buyOrderTimeoutWatchdog)
+            print()
+            print('!!SOLD {} ETH at ${} per Eth. (${})'.format(sellSize, sellPrice, round(float(sellPrice)*float(sellSize),2)))
+            lastSellPrice = sellPrice
+            lastBuySize = sellSize
     
     time.sleep(loopPeriod) 
         
